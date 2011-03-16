72833e7612fd8f29e9161b68519b83be53131524
Nathan Broadbent <nathan.f77@gmail.com>
2011-03-17 00:59:22 +1300

    Fixed schedule file.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,12 +1,15 @@
 # ---------------------------------------------------------
 #                    XBMC Schedule
 # ---------------------------------------------------------
+def xbmc(cmd)
+  command "cd /opt/scripts/xbmc_scripts/current && rake #{cmd}"
+end
 
 every 1.day, :at => '7:20 am' do
-  rake "play url=lastfm://globaltags/worship"
+  xbmc "play url=lastfm://globaltags/worship volume=40"
 end
 
 every 1.day, :at => '8:30 am' do
-  rake "audio:pause"
+  xbmc "audio:pause"
 end
 
