c7d52e884b17d6634c17a7726770054b8d1417ee
Nathan Broadbent <nathan.f77@gmail.com>
2011-03-17 01:34:06 +1300

    Made weekly music schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,11 +5,18 @@ def xbmc(cmd)
   command "cd /opt/scripts/xbmc_scripts/current && rake #{cmd}"
 end
 
-every 1.day, :at => '7:20 am' do
+work_day = [:tuesday, :wednesday, :thursday, :friday, :saturday]
+
+# Play radio on workday mornings for an hour.
+every work_day, :at => '7:20 am' do
   xbmc "play url=lastfm://globaltags/worship volume=40"
 end
-
-every 1.day, :at => '8:30 am' do
+every work_day, :at => '8:30 am' do
   xbmc "audio:pause"
+end
+
+# Chill-out on a monday morning.
+every :monday, :at => '9:15 am' do
+  xbmc "play url=lastfm://globaltags/chillout volume=25"
 end
 
